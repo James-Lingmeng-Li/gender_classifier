693126a8e6d106cebd5cc173a1227ae52c55b08d
bgadberr <bgadberr@users.noreply.github.com>
2018-05-03 19:59:35 -0400

    enter

--- gender_classifier.py
+++ gender_classifier.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy import sparse
 import csv
 from sklearn import tree
+from sklearn.linear_model import SGDClassifier
+from sklearn import svm
 
 # gender
 Y = []
@@ -28,25 +30,46 @@ genderFile.close()
 
 # initialize classifier
 clf = tree.DecisionTreeClassifier()
+clf2 = SGDClassifier()
+clf3 = svm.SVC()
 
 # train classifier using data set
 clf = clf.fit(X, Y)
+clf2 = clf2.fit(X, Y)
+clf3 = clf3.fit(X, Y)
 
 # enter user values for  classifier
-height = input('What is your height? (cm)')
-weight = input('What is your weight? (kg)')
-shoe_size = input('What is your shoe size? (eu)')
+height = float(input('What is your height? (cm)'))
+weight = float(input('What is your weight? (kg)'))
+shoe_size = float(input('What is your shoe size? (eu)'))
 
-#classifier prediction
+# classifier prediction
 prediction = clf.predict([[height, weight, shoe_size]])
+prediction2 = clf2.predict([[height, weight, shoe_size]])
+prediction3 = clf3.predict([[height, weight, shoe_size]])
+
+# display prediction
 gender = prediction[0]
+gender2 = prediction2[0]
+gender3 = prediction3[0]
+print("Decision Tree - " + gender)
+print("SGD Classifier - " + gender2)
+print("Linear SVC - " + gender3)
+
+# determine result
+result = ''
+if gender == gender2 or gender == gender3:
+    result = gender
+else:
+    result = gender3
+
 
 # write user values to data set if prediction is correct
-validation = input('Is ' + gender + ' correct? (y/n)')
+validation = input('Is ' + result + ' correct? (y/n)')
 if validation == "y":
     genderFile = open('gender.csv','a',newline='')
     genderWriter = csv.writer(genderFile)
-    genderWriter.writerow([gender,height,weight,shoe_size])
+    genderWriter.writerow([result,height,weight,shoe_size])
     genderFile.close()
 else:
     exit()
