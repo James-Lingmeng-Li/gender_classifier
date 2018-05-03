11da425f91a1f90382bc934a8a4551f825172d03
bgadberr <bgadberr@users.noreply.github.com>
2018-05-02 21:32:43 -0400

    enter

--- gender_decision_tree.py
+++ gender_decision_tree.py
@@ -2,7 +2,6 @@ from sklearn import tree
 import numpy as np
 from scipy import sparse
 import csv
-import sys
 
 # gender
 independent_list = []
@@ -10,34 +9,31 @@ independent_list = []
 # [height, weight, shoe_size]
 dependent_list = []
 
+# read CSV file
 genderFile = open('gender.csv')
 genderReader = csv.reader(genderFile)
 
+# skip header
 iter_genderReader = iter(genderReader)
 next(iter_genderReader)
 
+# populate lists
 for row in iter_genderReader:
    independent_list.append(row[0])
    dependent_list.append(row[1:])
 
-
+# instantiate the classifier
 clf = tree.DecisionTreeClassifier()
 
-
-# train algorithm on the data
+# train the classifier using the data
 clf = clf.fit(dependent_list, independent_list)
 
-height = int(input('What is your height? (cm)'))
-weight = int(input('What is your weight? (kg)'))
-shoe_size = int(input('What is your shoe size?'))
+# enter prediction values
+height = input('What is your height? (cm)')
+weight = input('What is your weight? (kg)')
+shoe_size = input('What is your shoe size? (eu)')
 
-
+#classifier prediction
 prediction = clf.predict([[height, weight, shoe_size]])
 
 print(prediction)
-
-
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2 
-# 3
